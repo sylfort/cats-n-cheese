36157802a15060f8b2c3f895bc48dfab4e107ae6
dominic-source <chinonsodomnic@gmail.com>
2024-06-21 14:31:14 +0100

    create file cat.js and add class Cat in it

---
+++ src/utils/cat.js
@@ -0,0 +1,27 @@
+export default class Cat {
+  constructor(name) {
+    this.id = 0;
+    this.name = name;
+    this._amountOfCheese = 0
+  }
+
+  get amountOfCheese() {
+    return this._amountOfCheese;
+  }
+
+  set amountOfCheese(cats) {
+    this._amountOfCheese =  this._randomAmountOfCheese(cats);
+  }
+
+  _randomAmountOfCheese(cats) {
+    return Math.floor(Math.random() * cats.length) + 1;
+  }
+
+  resetAmountOfCheese(cats) {
+    this.amountOfCheese = cats;
+  }
+
+  increaseAmountOfCheese(cats) {
+    this._amountOfCheese += this._randomAmountOfCheese(cats);
+  }
+}
